dd3e2fd56b4761b11fe7df91a582eb8bb01c76e8
Alan Hurtarte <alan@lionmane.io>
2019-05-07 19:03:56 -0600

    First commit. Movies APP. MVC.

---
+++ Data/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Movie> Movie { get; set; }
+    }
+}
